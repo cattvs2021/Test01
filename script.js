2cd6e0263176ebe7e9e23755bb688f297c45b909
“cattvs2021” <cattvs2021@gmail.com>
2022-10-05 13:11:50 -0500

    Grabbed file from school PC.

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello Catherine')
+
+var button1 = document.getElementById('button1');
+var pinkocean = document.getElementById('pinkocean');
+console.log(pinkocean);
+
+function imageChange() {
+    pinkocean.src = "pinkrocks.jpeg"
+}
